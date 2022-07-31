99f68a9c85e5ea2cb2ec71707b40f93f950e1f27
ZakM3 <zakaria.mahamed1@gmail.com>
2022-07-31 17:15:10 -0500

    first commit

---
+++ functions.test.ts
@@ -0,0 +1,7 @@
+const {shuffleArray} = require('./utils')
+
+describe('shuffleArray should', () => {
+test("shuffleArray should return same length as argument sent in", () => {
+    expect(shuffleArray.length).toEqual([]);
+})
+})
---
+++ heroku.md
@@ -0,0 +1,4 @@
+## Paste your link below
+
+
+https://trello.com/b/sWETHS9D/assessment-6
